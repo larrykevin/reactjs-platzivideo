86ba47c71218bc231f1bf4b743f5b742961cd851
Larry Caycho <larry.kevin.c.c@gmail.com>
2021-07-30 17:06:37 -0500

    Modificando archivo de configuración webpack

--- webpack.config.js
+++ webpack.config.js
@@ -44,6 +44,17 @@ module.exports = {
                 test: /\.css$/i,
                 use: [MiniCssExtractPlugin.loader, "css-loader"],
             },
+            {
+                test: /\.(png|jpe?g|gif)$/i,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: 'assets/[hash].[ext]',
+                        },
+                    },
+                ],
+            },
         ]
     },
     plugins: [
