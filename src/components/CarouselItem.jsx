959d30ffc0477d303db4477e52ca0a9616c00154
Larry Caycho <larry.kevin.c.c@gmail.com>
2021-08-01 18:53:21 -0500

    Modificando estilos de componente CarouselItem.jsx

--- src/components/CarouselItem.jsx
+++ src/components/CarouselItem.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import '../assets/styles/components/CarouselItem.scss';
 import playIcon from '../assets/static/play-icon.png';
 import plusIcon from '../assets/static/plus-icon.png';
 
-const CarouselItem = () => (
+const CarouselItem = ({ cover, title, year, contentRating, duration }) => (
   <div className="carousel-item">
     <div className="carousel-item__container">
       <img
-          alt=""
           className="carousel-item__img"
-          src="https://images.pexels.com/photos/789822/pexels-photo-789822.jpeg?auto=format%2Ccompress&cs=tinysrgb&dpr=2&h=750&w=1260"
+          src={cover}
+          alt={title}
       />
       <div className="carousel-item__details">
           <div>
@@ -24,11 +25,21 @@ const CarouselItem = () => (
                 src={plusIcon}
               />
           </div>
-          <p className="carousel-item__details--title">Título descriptivo</p>
-          <p className="carousel-item__details--subtitle">2019 16+ 114 minutos</p>
+          <p className="carousel-item__details--title">{title}</p>
+          <p className="carousel-item__details--subtitle">
+            {`${year} ${contentRating} ${duration}`}
+          </p>
       </div>
     </div>
   </div>
 );
+
+CarouselItem.propTypes = {
+  cover: PropTypes.string,
+  title: PropTypes.string,
+  year: PropTypes.number,
+  contentRating: PropTypes.string,
+  duration: PropTypes.number
+}
 
 export default CarouselItem;
